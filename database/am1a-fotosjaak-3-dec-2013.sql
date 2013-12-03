ad539c04cc3bee584f15f366c352125d2a37c28f
Arjan de Ruijter <rra@mboutrecht.nl>
2013-12-03 10:48:07 +0100

    Database export toegevoegd

---
+++ database/am1a-fotosjaak-3-dec-2013.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Machine: localhost
+-- Genereertijd: 03 dec 2013 om 09:46
+-- Serverversie: 5.6.12-log
+-- PHP-versie: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databank: `am1a-fotosjaak`
+--
+CREATE DATABASE IF NOT EXISTS `am1a-fotosjaak` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `am1a-fotosjaak`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `faq`
+--
+
+CREATE TABLE IF NOT EXISTS `faq` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `question_english` text NOT NULL,
+  `question_dutch` text NOT NULL,
+  `answer_english` text NOT NULL,
+  `answer_dutch` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `faq`
+--
+
+INSERT INTO `faq` (`id`, `question_english`, `question_dutch`, `answer_english`, `answer_dutch`) VALUES
+(1, 'Is this game hard to play?', 'Is dit een moeilijk spel om te spelen?', 'Yes, this game is very hard to crack.', 'Ja, dit spel is mega moeilijk.'),
+(2, 'When is this game completed?', 'Wanneer heb je dit spel uitgespeeld?', 'The game is a never ending experience. Full of joy and pleasure.', 'De game komt nooit ten einde. Het maakt je tot een beter en gelukkiger mens.');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `login`
+--
+
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  `userrole` enum('customer','root','administrator','photographer','developer') NOT NULL DEFAULT 'customer',
+  `activated` enum('yes','no') NOT NULL DEFAULT 'no',
+  `activationdate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `login`
+--
+
+INSERT INTO `login` (`id`, `email`, `password`, `userrole`, `activated`, `activationdate`) VALUES
+(1, 'customer@gmail.com', 'geheim', 'customer', 'no', '2013-12-02 00:00:00'),
+(2, 'administrator@gmail.com', 'geheim', 'administrator', 'no', '2013-12-03 08:16:00'),
+(3, 'root@gmail.com', 'geheim', 'root', 'no', '2013-12-04 00:00:00'),
+(4, 'photographer@gmail.com', 'geheim', 'customer', 'no', '2013-12-05 12:20:28'),
+(5, 'developer@gmail.com', 'geheim', 'developer', 'yes', '2013-12-15 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `infix` varchar(50) NOT NULL,
+  `surname` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `addressnumber` varchar(10) NOT NULL,
+  `city` varchar(200) NOT NULL,
+  `zipcode` varchar(6) NOT NULL,
+  `country` varchar(300) NOT NULL,
+  `telephonenumber` varchar(10) NOT NULL,
+  `mobilephonenumber` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) NOT NULL,
+  `infix` varchar(20) NOT NULL,
+  `surname` varchar(200) NOT NULL,
+  `street` varchar(163) NOT NULL,
+  `house_number` int(4) NOT NULL,
+  `city` varchar(163) NOT NULL,
+  `zip_code` varchar(6) NOT NULL,
+  `birthday` date NOT NULL,
+  `sex` enum('male','female') NOT NULL,
+  `marital_status` enum('married','single') NOT NULL,
+  `favo_genre` enum('not_selected','rpg','adventure','horror','shooter','mmorpg','casual','educational','sport','simulation') NOT NULL DEFAULT 'not_selected',
+  `favo_game` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `userrole` enum('customer','admin','root') NOT NULL DEFAULT 'customer',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `infix`, `surname`, `street`, `house_number`, `city`, `zip_code`, `birthday`, `sex`, `marital_status`, `favo_genre`, `favo_game`, `email`, `password`, `userrole`) VALUES
+(39, 'Admin', 'de', 'Admin', 'Adminstraat', 17, 'Adminstraat', '1910CB', '1990-01-01', 'male', 'married', 'adventure', 'IloMilo', 'admin@gmail.com', 'geheim', 'customer'),
+(40, 'customer', 'de', 'customer', 'customerstraat', 17, 'Customerstad', '1910CB', '1990-01-01', 'male', 'married', 'horror', 'IloMilo', 'customer@gmail.com', 'geheim', 'customer'),
+(41, 'root', 'de', 'root', 'rootsstraat', 12, 'rootstad', '1901CB', '1990-01-01', 'male', 'married', 'shooter', 'IloMilo', 'root@gmail.com', 'geheim', 'customer');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
