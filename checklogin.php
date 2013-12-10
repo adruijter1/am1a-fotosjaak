97112d2ded9eb0eb5a9f9bda8bc956043b030c41
Arjan de Ruijter <rra@mboutrecht.nl>
2013-12-10 11:46:31 +0100

    Er wordt gechecked of het account van degene die inlogt wel geactiveerd is. Wanneer dit niet het geval is dan wordt deze persoon doorgestuurd naar de algemene homepage

--- checklogin.php
+++ checklogin.php
@@ -3,6 +3,10 @@
 	//  dan moet je deze class eerst toevoegen met require_once
 	require_once("class/LoginClass.php");
 	
+	// Als je methoden uit de SessionClass wilt gebruiken
+	//  dan moet je deze class eerst toevoegen met require_once
+	require_once("class/SessionClass.php");
+	
 	//Check of beide velden zijn ingevoerd	
 	if ( !empty($_POST['email']) && !empty($_POST['password']))
 	{
@@ -10,27 +14,50 @@
 		if (LoginClass::check_if_email_password_exists($_POST['email'],
 													   $_POST['password']))
 		{
-			//echo "De combinatie bestaat";exit();	
-			//Verwijs door naar de homepage van de geregistreerde gebruiker
-			//echo "Record bestaat in de database";
-			$user_object = 
-				LoginClass::find_user_by_email_password($_POST['email'],
-														$_POST['password']);
-						
-			$_SESSION['id'] = $user_object->get_id();
-			$_SESSION['userrole'] = $user_object->get_userrole();
+			/* Check nu duidelijk is dat de combinatie van email en password
+			 * bestaat of het account wel geactiveerd is.			 * 
+			 */
+			if (LoginClass::check_if_account_is_activated($_POST['email'],
+														  $_POST['password']))
+		    {
 			
-			switch ($_SESSION['userrole'])
+				/* Roep de static method find_user_by_email_password aan uit
+				 * de LoginClass. Deze method geeft precies 1 LoginClass-object
+				 * terug. Je kunt via dit object de properties opvragen zoals:
+				 * get_id(), get_email(), get_password, enz......
+				 * 
+				 * Geef dit object vervolgens mee aan de method login($userObject)
+				 * uit de SessionClass. 
+				 */  								
+				$session->
+					login(LoginClass::find_user_by_email_password($_POST['email'],
+																  $_POST['password']));
+				
+				switch ($_SESSION['userrole'])
+				{
+					case 'root':
+						header("location:index.php?content=root_homepage");
+					break;
+					case 'administrator':
+						header("location:index.php?content=admin_homepage");			
+					break;
+					case 'customer':
+						header("location:index.php?content=customer_homepage");
+					break;
+					case 'developer':
+						header("location:index.php?content=developer_homepage");
+					break;
+					case 'photographer':
+						header("location:index.php?content=photographer_homepage");
+					break;			
+				}
+			}
+			else
 			{
-				case 'root':
-					header("location:index.php?content=root_homepage");
-				break;
-				case 'admin':
-					header("location:index.php?content=admin_homepage");			
-				break;
-				case 'customer':
-					header("location:index.php?content=customer_homepage");
-				break;			
+				echo "Uw account is nog niet door u geactiveerd. Check uw<br>
+					  email voor het klikken op de activatielink";
+				header("refresh:4; url=index.php?content=homepage");
+				
 			}
 		}
 		else
