63fdaedd5fdf3c468da86a641011c4201c7beb4e
Arjan de Ruijter <rra@mboutrecht.nl>
2014-01-07 10:54:39 +0100

    activationpagina gemaakt

---
+++ activation.php
@@ -0,0 +1,29 @@
+<p>Welkom op de site. Uw account wordt geactiveerd nadat<br> u een nieuw wachtwoord heeft gekozen</p>
+<form action='' method='post'>
+	<table>
+		<tr>
+			<td>nieuw wachtwoord(maximaal 12 tekens)</td>
+			<td>
+				<input type='text'
+					   name='password'
+					   size='12'
+					   maxlength='12'/>
+			</td>
+		</tr>
+		<tr>
+			<td>nieuw wachtwoord (check)</td>
+			<td><input type='text' 
+					   name='password-check' 
+					   size='12' 
+					   maxlength='12' />
+			</td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td>
+				<input type='submit' value='verstuur' />				
+			</td>
+		</tr>
+		
+	</table>	
+</form>
