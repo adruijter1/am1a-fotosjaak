ece0e61e402f47b4edde1a070a6eb623e562073d
Arjan de Ruijter <rra@mboutrecht.nl>
2014-03-04 11:32:25 +0100

    find_orders_by_user_id() method gemaakt in de order class

--- bekijk_opdracht.php
+++ bekijk_opdracht.php
@@ -1,13 +1,9 @@
 <?php 
 	$userrole = array('root', 'customer');
 	include("security.php");
+	require_once('class/OrderClass.php');
 ?>
 <p>Hieronder staan uw opdrachten</p>
-
-
-
-
-
 
 <table>
  <tr>
@@ -18,19 +14,6 @@
  	<th>kleur z/w</th>
  	<th>bekijk</th>
  </tr>
- <tr>
- 	<td>1</td>
- 	<td>De witte haai</td>
- 	<td>23-04-2014</td>
- 	<td>450</td>
- 	<td>color</td> 
- 	<td>
- 	  <a href=''>
- 		<img src='images/show_fotos.png'
- 			 alt='Bekijk de fotos' />
- 	  </a>
- 	</td>	
- </tr>
- <?php OrderClass::find_orders_by_user_id();  ?>
+ 	<?php OrderClass::find_orders_by_user_id();  ?>
 </table>
 
