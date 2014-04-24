688a6ccba9453865a46858f925724acb48d3bc0a
Arjan de Ruijter <rra@mboutrecht.nl>
2014-04-24 10:54:54 +0200

    Volgorde verandert van JQuery library

--- index.php
+++ index.php
@@ -16,10 +16,14 @@
 		<script style='text/javascript'
 			    src='./jquery/jquery-1.11.0.min.js'></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		
+		<script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>
+		
+		
 		<!-- Hier wordt de jquery ui bibliotheek toegevoegd aan de site -->
 		<script 			src="jquery/jquery-ui-1.10.4.custom/js/jquery-ui-1.10.4.custom.min.js">			
 		</script>
-		<script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>	
+			
 	
 	</head>
 	<body >
