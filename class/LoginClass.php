481656e9e3b917dc7bed9c66eb62d77f997fc4e1
Daniël van Drunen <daniel@pixely.me>
2018-07-07 04:14:01 +0200

    Replace deprecated MySQL functions with their MySQLi counterpart(s)

--- class/LoginClass.php
+++ class/LoginClass.php
@@ -12,7 +12,7 @@
 		private $userrole;
 		private $activated;
 		private $activationdate;
-		
+
 		// Properties
 		public function get_id() { return $this->id;}
 		public function get_email() { return $this->email;}
@@ -20,30 +20,30 @@
 		public function get_userrole() { return $this->userrole;}
 		public function get_activated() { return $this->activated;}
 		public function get_activationdate() { return $this->activationdate;}
-		
+
 		//Constructor
 		public function __construct()
 		{
-			
+
 		}
-		
+
 		public static function find_by_sql($query)
 		{
 			// Met global maak je het database-objct uit de MySqlDatabaseclass
 			// bruikbaar binnen de find_by_sql method
 			global $database;
-			
+
 			//Vuur de query af op de database
 			$result = $database->fire_query($query);
-			
+
 			/* Dit is het array waar alle LoginClassobjecten in worden gestopt
 			 * Elk LoginClassobject bevat een gevonden record uit de database
-			 * Vindt de query 3 records dan zitten er 3 LoginClassobjecten in 
-			 * het array $object_array. 
+			 * Vindt de query 3 records dan zitten er 3 LoginClassobjecten in
+			 * het array $object_array.
 			 */
 			$object_array = array();
-			
-			while ( $row = mysql_fetch_array($result))
+
+			while ( $row = mysqli_fetch_array($result))
 			{
 				// Maak iedere while ronde een LoginClassobject aan
 				$object = new LoginClass();
@@ -56,68 +56,68 @@
 				$object->activated		=	$row['activated'];
 				$object->activationdate	=	$row['activationdate'];
 				// Stop het net gemaakte LoginClassobject in het $object_array
-				$object_array[] = $object;			
+				$object_array[] = $object;
 			}
 			return $object_array;
 		}
-		
+
 		/* Deze method selecteert alle records uit de login table. We maken
 		 * gebruik van de find_by_sql($query) method uit deze class.
-		 */ 
+		 */
 		public static function select_all_from_login()
 		{
 			$query = "SELECT * FROM `login`";
 			$result = self::find_by_sql($query);
-			return $result;			
+			return $result;
 		}
-		
+
 		public static function email_exists($emailaddress)
 		{
-			global $database;	
-			$query = "SELECT * 
-					  FROM `login` 
+			global $database;
+			$query = "SELECT *
+					  FROM `login`
 					  WHERE `email` = '".$emailaddress."'";
 			$result = $database->fire_query($query);
-			if ( mysql_num_rows($result) > 0)
+			if ( mysqli_num_rows($result) > 0)
 			{
 				return "Het e-mailadres bestaat al in de database";
 			}
 			else
 			{
 				return "Het e-mailadres bestaat niet in de database";
-			}			
+			}
 		}
-		
+
 		public static function check_if_email_password_exists($email, $password)
 		{
 			global $database;
-			$query = "SELECT * 
+			$query = "SELECT *
 					  FROM `login`
 					  WHERE `email` = '".$email."'
 					  AND `password` = '".$password."'";
 			//echo $query; exit();
 			$result = $database->fire_query($query);
-			if (mysql_num_rows($result) > 0)
+			if (mysqli_num_rows($result) > 0)
 			{
 				return true;
 			}
-			else 
+			else
 			{
 				return false;
 			}
 		}
-		 
+
 		public static function find_user_by_email_password($email, $password)
 		{
-			$query = "SELECT * 
+			$query = "SELECT *
 					  FROM `login`
 					  WHERE `email` = '".$email."'
 					  AND `password` = '".$password."'";
 			//echo $query; exit();
-			$loginClassObjectInArray = self::find_by_sql($query);	
+			$loginClassObjectInArray = self::find_by_sql($query);
 			return $loginClassObject = array_shift($loginClassObjectInArray);
 		}
-		
+
 		public static function check_if_account_is_activated($email,
 															 $password)
 		{
@@ -127,7 +127,7 @@
 					  FROM		`login`
 					  WHERE		`email`		=	'".$email."'
 					  AND		`password`	=	'".$password."'";
-				
+
 			// Vuur de query af op de database met de static method
 			// find_by_sql($query)
 			$object_array = self::find_by_sql($query);
@@ -136,26 +136,26 @@
 			{
 				return true;
 			}
-			else 
+			else
 			{
 				return false;
 			}
 		}
-		
+
 		public static function check_if_email_exists($email)
 		{
-			global $database;	
-				
+			global $database;
+
 			// Zet hier je commentaar
 			$query = "SELECT	*
 					  FROM		`login`
 					  WHERE		`email`	=	'".$email."'";
-			
+
 			// Zet hier je commentaar
 			$result = $database->fire_query($query);
-			
+
 			// Zet hier je commentaar
-			if (mysql_num_rows($result) > 0)
+			if (mysqli_num_rows($result) > 0)
 			{
 				return true;
 			}
@@ -163,22 +163,22 @@
 			{
 				return false;
 			}
-			
-			// Ternary operator variabele 
+
+			// Ternary operator variabele
 			// (vergelijking) ? waarde als waar : waarde als niet waar
 			// return (mysql_num_rows($result) > 0) ? true : false;
 		}
-		
+
 		public static function insert_into_loginClass($post_array)
 		{
 			global $database;
-						
+
 			$date = date("Y-m-d H:i:s");
-			
+
 			$tmp_password = $date.$post_array['email'];
-			
+
 			$hash_from_tmp_password = MD5($tmp_password);
-			
+
 			$query = "INSERT INTO	`login` (`id`,
 											 `email`,
 											 `password`,
@@ -192,18 +192,18 @@
 					  						 'no',
 					  						 '".$date."')";
 			$database->fire_query($query);
-			
+
 			$id = mysql_insert_id();
-			
+
 			UserClass::insert_into_userClass($post_array, $id);
 			self::send_activation_email($post_array, $hash_from_tmp_password);
 		}
-		
+
 		public static function send_activation_email($post_array, $password)
 		{
 			$to = $post_array['email'];
 			$subject = "Activatie website FotoSjaak";
-			
+
 			/*
 			$message = "Geachte heer/mevrouw ".
 					   $post_array['firstname']." ".
@@ -214,9 +214,9 @@
 			$message .= "http://localhost/2013-2014/Blok2/AM1A/fotosjaak-am1a/index.php?content=activation&email=".$post_array['email']."&password=".$password."\r\n";
 			$message .= "Met vriendelijke groet,\r\n";
 			$message .= "Sjaak de Vries\r\n";
-			$message .= "Uw fotograaf";	
+			$message .= "Uw fotograaf";
 			*/
-			
+
 			$message = "<p><u>Geachte heer/mevrouw <b>".
 					   $post_array['firstname']." ".
 					   $post_array['infix']." ".
@@ -226,8 +226,8 @@
 			$message .= "<a href='http://localhost/2013-2014/Blok2/AM1A/fotosjaak-am1a/index.php?content=activation&email=".$post_array['email']."&password=".$password."'>activeer account</a><br><br>";
 			$message .= "Met vriendelijke groet,<br>";
 			$message .= "Sjaak de Vries<br>";
-			$message .= "Uw fotograaf";	
-			
+			$message .= "Uw fotograaf";
+
 			$headers  = "From: info@fotosjaak.nl\r\n";
 			$headers .=	"Reply-To: info@fotosjaak.nl\r\n";
 			$headers .= "Cc: sjaak@fotosjaak.nl\r\n";
@@ -243,35 +243,35 @@
 											 			$password)
 		{
 			global $database;
-			
+
 			$date = date("Y-m-d H:i:s");
-			
+
 			$query = "UPDATE `login`
 					  SET 	 `password`			= '".$password."',
 					  	  	 `activated`		= 'yes',
 					  	  	 `activationdate`	= '".$date."'
 					  WHERE  `email`			= '".$email."'";
-			$database->fire_query($query);				
+			$database->fire_query($query);
 		}
-		
+
 		public static function find_email_password_by_id()
 		{
 			$query = "SELECT *
-					  FROM `login` 
+					  FROM `login`
 					  WHERE `id` = '".$_SESSION['id']."'";
 			$login_array = self::find_by_sql($query);
 			$login_object = array_shift($login_array);
-			return $login_object;			
+			return $login_object;
 		}
-		
+
 		public static function check_if_email_password_matches_md5($email, $password)
 		{
 			global $database;
-			$query = "SELECT * 
+			$query = "SELECT *
 					  FROM `login`
 					  WHERE `email` = '".$email."'";
 			$result = self::find_by_sql($query);
-			
+
 			$login_object = array_shift($result);
 			// Check of het emailadres bestaat
 			if ( $login_object != null)
@@ -282,15 +282,15 @@
 				{
 					return true;
 				}
-				else 
+				else
 				{
 					return false;
 				}
 			}
-			else 
+			else
 			{
 				return false;
 			}
-		}										
+		}
 	}
 ?>
--- class/MySqlDatabaseClass.php
+++ class/MySqlDatabaseClass.php
@@ -5,7 +5,7 @@
 	{
 		//Fields
 		private $db_connection;
-		
+
 		/* Dit is de constructor van de MySqlDatabaseClass
 		 * Een constructor herken je in PHP aan de naam. De naam is altijd
 		 *  __construct
@@ -13,25 +13,25 @@
 		public function __construct()
 		{
 			// Velden roep je aan met $this->naam_van_het_veld_zonder_$
-			// Er wordt hier een verbinding gemaakt met de mysql-server				
-			$this->db_connection = mysql_connect(SERVERNAME,
+			// Er wordt hier een verbinding gemaakt met de mysql-server
+			$this->db_connection = mysqli_connect(SERVERNAME,
 												 USERNAME,
 												 PASSWORD);
-			// Er wordt hier een database geselecteerd								 
-			mysql_select_db(DATABASE, $this->db_connection) or 
+			// Er wordt hier een database geselecteerd
+			mysqli_select_db($this->db_connection, DATABASE) or
 				die('MySqlDatabaseClass, database niet geselecteerd');
 		}
-		
+
 		// De functie krijgt als argument een query mee.
 		// Deze wordt door de mysql_query($query) afgevuurd op de database
 		public function fire_query($query)
 		{
-			$result = mysql_query($query) 
-						or die('MySqlDatabase: '.mysql_error());
+			$result = mysqli_query($this->db_connection, $query)
+						or die('MySqlDatabase: '.mysqli_error($this->db_connection));
 			return $result;
-		}		
+		}
 	}
-	
+
 	// Maak nu een instantie van de net gedefinieerde class.
 	$database = new MySqlDatabaseClass();
 ?>
